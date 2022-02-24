2e4f8de61d220558464e422cb1f91deb04e53c26
Greg Marks <g123616@byu.edu>
2022-02-23 23:39:27 -0700

    Beginning changes

--- FinalMission7/Startup.cs
+++ FinalMission7/Startup.cs
@@ -51,6 +51,11 @@ namespace FinalMission7
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "Paging",
+                    pattern: "Page{pageNum}",
+                    defaults: new { Controller = "Home", action= "Index"});
+
                 endpoints.MapDefaultControllerRoute();
             });
         }
